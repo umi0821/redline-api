ef6575d5815bf32ee62587e263c4cec14d8c1219
umi0821 <hitohosiena@gmail.com>
2025-04-24 11:17:32 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import StreamingResponse
 import cv2
 import numpy as np
 import io
-from fastapi.responses import StreamingResponse
+import json
 
 app = FastAPI()
 
-@app.post("/draw-line")
-async def draw_red_line(file: UploadFile = File(...)):
+@app.post("/draw-lines")
+async def draw_red_lines(file: UploadFile = File(...), lines: str = Form(...)):
     contents = await file.read()
     npimg = np.frombuffer(contents, np.uint8)
     img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
-    # 赤線を描く
-    cv2.line(img, (50, 50), (300, 50), (0, 0, 255), 5)
+    line_data = json.loads(lines)
+
+    for line in line_data:
+        start = tuple(line["start"])
+        end = tuple(line["end"])
+        cv2.line(img, start, end, (0, 0, 255), 3)
 
     _, encoded_img = cv2.imencode('.jpg', img)
     return StreamingResponse(io.BytesIO(encoded_img.tobytes()), media_type="image/jpeg")
+
